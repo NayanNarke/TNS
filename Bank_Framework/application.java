3188f43cebdb29daa878e9cc3da6aa8a5838e5dd
NayanNarke <102030318+NayanNarke@users.noreply.github.com>
2023-11-25 21:43:46 +0530

    Update application.java

--- Bank_Framework/application.java
+++ Bank_Framework/application.java
@@ -3,8 +3,8 @@ package Bank_Framework;
 public class application {
 	public static void main(String arg[]) {
 		BankFactory b=new MMBankFactory();
-		SavingAcc sa=b.getNewSavingAcc(21, "Sumesh", 20000, true);
-		CurrentAcc ca=b.getNewCurrentAcc(23, "Unni", 220, 700);
+		SavingAcc sa=b.getNewSavingAcc(21, "Nayan", 20000, true);
+		CurrentAcc ca=b.getNewCurrentAcc(23, "Sumesh", 220, 700);
 		
 		System.out.println("Total Balance of Saving account before withdraw is : " +sa.getAccBal());
 		System.out.println("Total Balance of Current account  before withdraw is : " +ca.getAccBal());
